d160b8b7d4746ccc506ec151146ee9e9cd288e42
Chris Zheng <z@caudate.me>
2022-11-08 00:38:59 +0800

    adding entrypoint

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash                                                                                                                                     
 
+
+ln -s /usr/bin/R /usr/bin/r
+ln -s /usr/bin/python3 /usr/bin/python
+service postgresql start
+redis-server &
 Xvfb :1 -screen 0 800x600x16 &
 /usr/bin/x11vnc -display :1.0 -usepw &
 export DISPLAY=:1.0
 export PATH=$HOME/bin:$PATH
-service postgresql start
-redis-server &
 echo "$@"
 eval "$@"
