3bb50e599b089a6b29aabd544130603ca09b5d04
Chris Zheng <z@caudate.me>
2022-11-07 19:11:21 +0800

    adding stuff

---
+++ infra/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash                                                                                                                                     
+
+Xvfb :1 -screen 0 800x600x16 &
+/usr/bin/x11vnc -display :1.0 -usepw &
+export DISPLAY=:1.0
+export PATH=$HOME/bin:$PATH
+echo "$@"
+eval "$@"
--- infra/project.clj
+++ infra/project.clj
@@ -1,13 +1,14 @@
 (defproject zcaudate/infra "4.0.1"
   :dependencies
   [;; dev
-   [org.clojure/clojure "1.10.3"]
-   [javax.xml.bind/jaxb-api "2.3.1"]
-   [com.sun.xml.bind/jaxb-core "3.0.2"]
-   [com.sun.xml.bind/jaxb-impl "3.0.2"]
+   [org.clojure/clojure "1.11.1"]
+   [nrepl "1.0.0"]
+   [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
+   [com.sun.xml.bind/jaxb-core "4.0.1"]
+   [com.sun.xml.bind/jaxb-impl "4.0.1"]
    
    ;; code.doc
-   [markdown-clj/markdown-clj "1.10.6"] ;; not mustache
+   [markdown-clj/markdown-clj "1.11.3"] ;; not mustache
 
    ;; code.manage
    [org.clojure/tools.reader "1.3.6"]
@@ -38,6 +39,14 @@
    [org.eclipse.aether/aether-transport-classpath "1.1.0"]
    [org.apache.maven/maven-aether-provider "3.3.9"]
 
+   ;; lib.antlr
+   ;; [org.antlr/antlr4-runtime "4.11.1"]
+   ;; [org.antlr/antlr4 "4.11.1"]
+   ;; [thi.ng/structgen "0.2.1"]
+   
+   ;; rt.janino
+   [org.codehaus.janino/janino "3.1.8"]
+
    ;; lib.lucene
    [org.apache.lucene/lucene-core "8.10.0"]
    [org.apache.lucene/lucene-queryparser "8.10.0"]
@@ -45,30 +54,36 @@
    [org.apache.lucene/lucene-suggest "8.10.0"]
 
    ;; lib.openpgp
-   [org.bouncycastle/bcprov-jdk15on "1.65"]
-   [org.bouncycastle/bcpg-jdk15on "1.65"]
+   [org.bouncycastle/bcprov-jdk15on "1.70"]
+   [org.bouncycastle/bcpg-jdk15on "1.70"]
 
    ;; lib.oshi
-   [com.github.oshi/oshi-core "5.8.2"]
+   [com.github.oshi/oshi-core "6.3.1"]
 
    ;; math.stat
    [net.sourceforge.jdistlib/jdistlib "0.4.5"]
 
    ;; math.infix
-   [org.scijava/parsington "2.0.1"]
+   [org.scijava/parsington "3.0.0"]
    
    ;; rt.jocl
    [org.jocl/jocl "2.0.4"]
 
+   ;; rt.jep
+   [black.ninia/jep "4.1.0"]
+
    ;; rt.graal
-   [org.graalvm.sdk/graal-sdk "21.2.0"]
-   [org.graalvm.truffle/truffle-api "21.2.0"]
-   [org.graalvm.js/js "21.2.0"]
-   [org.graalvm.js/js-scriptengine "21.2.0"]
+   [org.graalvm.sdk/graal-sdk "22.3.0"]
+   [org.graalvm.truffle/truffle-api "22.3.0"]
+   [org.graalvm.js/js "22.3.0"]
+   [org.graalvm.js/js-scriptengine "22.3.0"]
    [commons-io/commons-io "2.11.0"]
 
    ;; rt.postgres
    [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.8.9"]
+   
+   ;; rt.wasm
+   [io.helins/wasm "0.0.0-alpha3"]
    
    ;; rt.websocket
    [http-kit "2.5.3"]
@@ -87,13 +102,13 @@
    [com.moandjiezana.toml/toml4j "0.7.2"]
 
    ;; script.yaml
-   [org.yaml/snakeyaml "1.29"]
+   [org.yaml/snakeyaml "1.33"]
 
    ;; std.fs.archive
    [org.apache.commons/commons-compress "1.21"]
 
    ;; std.config
-   [borkdude/edamame "0.0.11"]
+   [borkdude/edamame "1.0.0"]
 
    ;; std.contract
    [metosin/malli "0.2.1"]
@@ -102,15 +117,15 @@
    [org.jsoup/jsoup "1.14.2"]
 
    ;; std.image
-   [com.twelvemonkeys.imageio/imageio-bmp  "3.7.0"]
-   [com.twelvemonkeys.imageio/imageio-tiff "3.7.0"]
-   [com.twelvemonkeys.imageio/imageio-icns "3.7.0"]
-   [com.twelvemonkeys.imageio/imageio-jpeg "3.7.0"]
+   [com.twelvemonkeys.imageio/imageio-bmp  "3.9.3"]
+   [com.twelvemonkeys.imageio/imageio-tiff "3.9.3"]
+   [com.twelvemonkeys.imageio/imageio-icns "3.9.3"]
+   [com.twelvemonkeys.imageio/imageio-jpeg "3.9.3"]
 
    ;; std.json
-   [com.fasterxml.jackson.core/jackson-core "2.12.5"]
-   [com.fasterxml.jackson.core/jackson-databind "2.12.5"]
-   [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.12.5"]
+   [com.fasterxml.jackson.core/jackson-core "2.14.0"]
+   [com.fasterxml.jackson.core/jackson-databind "2.14.0"]
+   [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.14.0"]
 
    ;; std.math
    [org.apache.commons/commons-math3 "3.6.1"]
@@ -118,6 +133,7 @@
    ;; std.text.diff
    [com.googlecode.java-diff-utils/diffutils "1.3.0"]
    
+
    ;; TESTS
    [org.eclipse.jgit/org.eclipse.jgit "5.13.0.202109080827-r"]]
   :global-vars {*warn-on-reflection* true}
