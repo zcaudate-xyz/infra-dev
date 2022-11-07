6ac1c8a28bf872fc9b2b9c389ff49c33ca13f91c
Chris Zheng <z@caudate.me>
2022-11-07 22:01:37 +0800

    adding stuff

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -4,7 +4,5 @@ Xvfb :1 -screen 0 800x600x16 &
 /usr/bin/x11vnc -display :1.0 -usepw &
 export DISPLAY=:1.0
 export PATH=$HOME/bin:$PATH
-service redis start
-service postgresql start
 echo "$@"
 eval "$@"
