319083de016bc4b9b406030207c9d4bfd81435f8
Chris Zheng <z@caudate.me>
2022-11-08 01:33:21 +0800

    fix

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash                                                                                                                                     
 
-
-ln -s /usr/bin/R /usr/bin/r
-ln -s /usr/bin/python3 /usr/bin/python
 service postgresql start
 redis-server &
 Xvfb :1 -screen 0 800x600x16 &
